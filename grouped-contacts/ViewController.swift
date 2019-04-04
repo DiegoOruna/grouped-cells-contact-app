84a3690ae04d47564409d01760c88c93008c3ecc
Diego Oruna Cabrera <diego.orunac@gmail.com>
2019-04-03 20:42:05 -0500

    Set up sections with two dimensional array

--- grouped-contacts/ViewController.swift
+++ grouped-contacts/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    
+    let twoDimensionalArray = [
+        ["Amy","Bill","Zack","Diego","Carlos","Jack","Steve","Mary","Cleveland"],
+        ["Juan","Jose","Julio","Marcos"],
+        ["David","Dan"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionalArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = .lightGray
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimensionalArray[section].count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+
+        let name = twoDimensionalArray[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row:\(indexPath.row)"
+        return cell
+    }
 
 }
 
